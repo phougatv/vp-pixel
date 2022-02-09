05263780e17e78459a62a3f6e6a6b7f934d4fabf
Vedant Phougat <vedant.phougat@hotmail.com>
2022-02-09 21:45:43 +0530

    Formatted Persistence extension class.

--- src/Core/Persistence/Extension.cs
+++ src/Core/Persistence/Extension.cs
@@ -7,11 +7,7 @@ using VP.Pixel.Core.Persistence.User;
 public static class Extension
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services)
-    {
-        services
+        => services
             .AddDbContext<UserDbContext>(dbContextOptionsBuilder => dbContextOptionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=VP.Pixel;Integrated Security=SSPI;Trusted_Connection=True"))
             .AddScoped<IUserRepository, UserRepository>();
-
-        return services;
-    }
 }

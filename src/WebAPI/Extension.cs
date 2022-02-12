48c6ac09563d9f5c92846ea26104751506d5582b
Vedant Phougat <vedant.phougat@hotmail.com>
2022-02-12 13:01:42 +0530

    Added pixel configuration method

---
+++ src/WebAPI/Extension.cs
@@ -0,0 +1,69 @@
+﻿namespace VP.Pixel.WebAPI;
+
+using VP.Pixel.Core.Persistence.Extensions;
+
+public static class Extension
+{
+    #region WebApplication Extensions
+    internal static WebApplicationBuilder CreatePixelBuilder()
+    {
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"
+        });
+
+        builder.Configuration.AddPixelAppSettings(builder.Environment.EnvironmentName, "./Json/appsettings.json");
+        builder.Services.AddPixelServices(builder.Configuration);
+
+        return builder;
+    }
+    #endregion WebApplicationBuilder Extensions
+
+    #region Startup Extensions
+    /// <summary>Adds services to the container during the startup of the application</summary>
+    /// <param name="services">The <see cref="IServiceCollection"/></param>
+    /// <returns></returns>
+    internal static void AddPixelServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddControllers();
+        services
+            .AddPersistence(configuration)
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen();
+    }
+
+    /// <summary>Configures the HTTP request pipeline</summary>
+    /// <param name="app">The <see cref="ApplicationBuilder"/></param>
+    /// <param name="env">The <see cref="IWebHostEnvironment"/></param>
+    /// <param name="logger">The <see cref="ILogger"/></param>
+    internal static void UseItToConfigurePixelRequestPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app
+                .UseSwagger()
+                .UseSwaggerUI();
+        }
+
+        app
+            .UseHttpsRedirection()
+            .UseAuthorization();
+
+        app.MapControllers();
+    }
+    #endregion Startup Extensions
+
+    #region Configuration Extensions
+    internal static void AddPixelAppSettings(
+        this ConfigurationManager manager,
+        String environmentName,
+        String filePath)
+    {
+        manager
+            .AddJsonFile($"{Path.ChangeExtension(filePath, "")}json", true, true)
+            .AddJsonFile($"{Path.ChangeExtension(filePath, "")}{environmentName}.json", true, true);
+    }
+    #endregion Configuration Extensions
+}
--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,17 +1,11 @@
 using VP.Pixel.WebAPI;
+using static VP.Pixel.WebAPI.Extension;
 
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions());
-builder.Host.ConfigureAppConfiguration(
-    (hostingContext, config) => config
-    .AddJsonFile("./Json/appsettings.json", true, true)
-    .AddJsonFile($"./Json/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development" }.json", true, true));
-
-// Add services to the container.
-builder.Services.AddPixelServices(builder.Configuration);
+var builder = CreatePixelBuilder();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseItToConfigureRequestPipeline();
+app.UseItToConfigurePixelRequestPipeline();
 
 app.Run();

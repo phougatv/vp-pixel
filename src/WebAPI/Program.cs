38c185a843e2acbc33dd4f008f03ca19ac089285
Vedant Phougat <vedant.phougat@hotmail.com>
2022-02-13 20:29:19 +0530

    Removed StartupExtension.cs

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,11 +1,34 @@
-using VP.Pixel.WebAPI;
-using static VP.Pixel.WebAPI.Extension;
+using VP.Pixel.Core.Persistence.Extensions;
 
-var builder = CreatePixelBuilder();
+//Add configuration & services to the DI container
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"
+});
+builder.Configuration
+    .AddJsonFile($"{Path.ChangeExtension("./Json/appsettings.json", "")}json", true, true)
+    .AddJsonFile($"{Path.ChangeExtension("./Json/appsettings.json", "")}{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Services
+    .AddControllers();
+builder.Services
+    .AddPersistence(builder.Configuration)
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseItToConfigurePixelRequestPipeline();
+if (app.Environment.IsDevelopment())
+{
+    app
+        .UseSwagger()
+        .UseSwaggerUI();
+}
+
+app
+    .UseHttpsRedirection()
+    .UseAuthorization();
+app.MapControllers();
 
 app.Run();
